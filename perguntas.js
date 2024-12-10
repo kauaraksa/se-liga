ec1cd277f57f4269f6ff3f9ed970a02ec605c302
kauaraksa <164026046+kauaraksa@users.noreply.github.com>
2024-12-10 08:16:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,42 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'civismo',
+    'O que é uma lei?',
+    'uma lei e uma regra oficial que todos devem seguir'
+)
+
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'robotica',
+    'quais são as cores dos leds',
+    'vermelho, verde, amarelo, Branco'
+)
+
+criaCartao(
+    'historia',
+    'o que aconteceu em 1945?',
+    'termino da segunda guerra mundial'
+)
+
+criaCartao(
+    'projeto de vida',
+    'oque é o racismo?',
+    'é o preconceito com o tom de pele'
+)
